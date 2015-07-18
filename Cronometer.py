7ff296dfa1934087b0e1f3470bca66759476366d
KaosJL <looterd@mailoo.org>
2015-07-18 02:10:22 -0600

    Clase documentada

--- Cronometer.py
+++ Cronometer.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 
 class Cronometer :
     """ Crea un objeto que gestiona una cuenta regresiva, que posteriormente
-        se visualizará en una superficie.
+        se visualizara en una superficie.
     """
 
     def __init__(self,time):
@@ -34,22 +34,40 @@ class Cronometer :
         return None
 
     def start_clock(self):
+        """
+        Metodo utilizado para reducir segundo a segundo con cada llamada el tiempo restantante.
+        Genera un objeto de tipo superfice en la que se imprime la cuenta, los parametros del
+        texto de la superficie se definen en el init de la clase.
+        :return: surface.
+        """
         self.seconds += self.reloj.tick(60)
         if self.seconds/1000 == 1 and not(self.lock_time) and self.get_remaining_time()> 0:
             self.remaining_t -= 1
             self.seconds = 0
-        print self.get_remaining_time()
+
         self.texto = self.fuente.render(str(self.get_remaining_time()),1,(255,255,255))
 
         return self.texto
 
     def pause_clock(self):
+        """
+        Metodo utilizado para pausar la cuenta regresiva. Se utiliza una variable de bloqueo llamada lock_time
+        para evitar que se continue restando los segundos de remaining_t.
+        lock_time cambia su valor a True indicando que la cuenta regresiva se para temporalmente
+        :return:None
+        """
         self.set_temp(self.get_remaining_time())
 
         self.lock_time = True
         return None
 
     def continue_clock(self):
+        """
+        Deshabilita el bloqueo que con anterioridad se colo con una llamada a pause_clock.
+        Asigna a lock_time el valor False, permitiendo a la cuenta regresiva continuar.
+
+        :return: None
+        """
         self.lock_time = False
         self.set_remaining_time(self.temp)
         self.start_clock()
