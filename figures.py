3538c37de23eca80d9d148ff26263c071bfe72f0
gabycp <gaby_cor93@hotmail.com>
2015-08-15 16:37:01 -0600

    metodo en mario para hacer clision

--- figures.py
+++ figures.py
@@ -12,7 +12,7 @@ import random
 class mario(pygame.sprite.Sprite):
     def __init__(self, imagesMario, posx, posy, soilLevel):
         pygame.sprite.Sprite.__init__(self)
-        self.resolution = (75, 87)
+        self.resolution = (60, 90)
         self.images = imagesMario
         self.soilLevel = soilLevel
         self.imageNumber = 0
@@ -49,7 +49,8 @@ class mario(pygame.sprite.Sprite):
         self.rect.move_ip(0, -move)
 
     def everDown(self):
-        self.rect.move_ip(0, 1)
+        if self.rect.top>548:
+          self.rect.move_ip(0, 1)
 
     def run(self, key):
         if (key == pygame.K_LEFT):
@@ -61,11 +62,11 @@ class mario(pygame.sprite.Sprite):
 
     def stopped(self):
         self.imageNumber = 0
-        self.image = pygame.transform.scale(self.images[self.imageNumber], self.resolution)
+        self.image = pygame.transform.scale(self.images[self.imageNumber],self.resolution)
 
     def jump(self):
         # solo evalúa un salto si está en el suelo.
-        self.initial = -28
+        self.initial = -30
         self.imageNumber = 4
         self.image = pygame.transform.scale(self.images[self.imageNumber], self.resolution)
         if self.invert:
@@ -73,8 +74,17 @@ class mario(pygame.sprite.Sprite):
 
     def updateJump(self):
         # si está saltando actualiza su posición
-        self.rect.y += self.initial
-        self.initial += 3
+            self.rect.y += self.initial
+            self.initial += 3
+
+    def collideBricks(self, mario, listBricks):
+        ubication = -1
+        self.mario = mario
+        self.listBricks = listBricks
+        for brick in self.listBricks:
+            if pygame.sprite.collide_mask(self.mario, brick):
+                ubication = self.listBricks.index(brick)
+        return ubication
 
 '''
     Esta clase representa el mario que se desplaza a traves de los niveles para seleccionar cual jugar
@@ -119,6 +129,7 @@ class main_mario(pygame.sprite.Sprite):
         if key == pygame.K_RIGHT:
             self.rect.move_ip(value, 0)
 
+
 '''
     Esta clase representa los bloques de nivel o mundo en el que se posicionará mario para entrar a el mismo
 '''
@@ -154,13 +165,14 @@ class level(pygame.sprite.Sprite):
 class brick(pygame.sprite.Sprite):
     def __init__(self, imagesBrick, posx, posy):
         pygame.sprite.Sprite.__init__(self)
-        self.resolution = (40, 50)
+        self.resolution = (50, 50)
         self.images = imagesBrick
         self.imageNumber = 0
         self.image = pygame.transform.scale(self.images[self.imageNumber], self.resolution)
         self.rect = self.images[self.imageNumber].get_rect()
         self.rect.left = posx
         self.rect.top = posy
+        self.listRect = []
 
     '''
         Este metodo tiene como fin modelar los comportamientos del bloque donde se para hammerbros, en su desplazamiento
@@ -325,56 +337,56 @@ class castle(pygame.sprite.Sprite):
         self.rect.top = posy
 
 class smallHammer(pygame.sprite.Sprite):
-    def __init__(self, image, posX, posY):
+    def __init__(self, imageH, posX, posY):
         # Intancializar la clase sprite
         pygame.sprite.Sprite.__init__(self)
-        # La posicion en x y la posicion en y, del martillo
-        self.posX = posX
-        self.posY = posY
         self.randomt = random.uniform(0.4, 2)
         # Cargamos la imagen del martillo
-        self.sHammer = image
+        self.image = imageH
+        self.size=(30,40)
+        self.image = pygame.transform.scale(self.image, self.size)
         # Dibuja una superficie segun el tamanio de la imagen
-        self.rectH = image.get_rect()
+        self.rect = self.image.get_rect()
         # Posicion inicial del martillo
-        self.rectH.center = [self.posX, self.posY]
+        self.rect.center = [posX, posY]
         # La gravedad que es cte
         self.gravity = 1
         # El tiempo
         self.timeH = self.randomt
         # Velocidad inicial
-        self.vO = 12
+        self.vO = 16
         # El angulo de 45 grados
-        self.angle = 38
+        self.angle = 30
+        self.rectHammer= []
 
     def throwHammer(self):
-        # Calcular la velocidad inicial en x
-        self.voX = self.vO * (math.cos((self.angle)))
+         # Calcular la velocidad inicial en x
+        self.voX = self.vO * (math.cos(math.radians(self.angle)))
         # Calcular la velcidad inicial en y
-        self.voY = self.vO * (math.sin((self.angle)))
+        self.voY = self.vO * (math.sin(math.radians(self.angle)))
         speed = [-(self.voX), (self.voY)]
         # Incrementa o disminuye la velocidad en y por la formula
-        speed[1] -= (self.gravity * self.timeH)
+        speed[1] -= (self.gravity * self.timeH * self.timeH)
         # Movimiento del martillo
-        self.rectH.move_ip(speed[0], speed[1])
+        self.rect.move_ip(speed[0], speed[1])
 
     def drawHammer(self, screen):
-        # pygame.time.wait(50)
-        # Dibuja el martillo en la pantalla
-        self.window.blit(self.sHammer, self.rectH)
-
+        self.image = pygame.transform.rotate(self.image, 90)
+        screen.blit(self.image, self.rect)
+        self.throwHammer()
 
 class enemy(pygame.sprite.Sprite):
-    # Inicializa la clase
-    def __init__(self, images, smallhammer, posX, posY):
+     # Inicializa la clase
+    def __init__(self, images, smallhammer, posX, posY, listBricks):
         # Intancializar la clase sprite
         pygame.sprite.Sprite.__init__(self)
         # Obtener lista de todas las imagenes del sprite
         self.listImages = images
+        # Tamaño del enemigo y el martillo
+        self.size = (60,70)
         # Imagen del martillo
-        self.imageHammer = smallHammer
-        # Separa cada imagen
-        self.listImages.sort()
+        self.imageSH = smallhammer
+        self.listB = self.rectBricks(listBricks)
         # Variable para la primera posicion que esta la imagen
         self.firstImage = 0
         # El tamanio del objeto en x
@@ -384,52 +396,87 @@ class enemy(pygame.sprite.Sprite):
         self.n = 1
         # Tamanio de la lista de imagenes
         self.lastImage = len(self.listImages) - 1
-        # Tamaño del enemigo
-        self.size = (60,70)
         # Carga la primera imagen
         self.image = pygame.transform.scale(self.listImages[0], self.size)
         self.image = pygame.transform.flip(self.image, True, False)
-        self.rectHammer = self.image.get_rect()
+        self.rect = self.image.get_rect()
+        self.rect.left = posX
         self.posX = posX
         self.posY = posY
+        self.rect.top = posY
         # Posicion de imagen en la pantalla
         self.position = [posX, posY]
-        # La pantalla
-        # Lista de hammers
-        self.listSmallHammers = []
+        self.listSmallHammers = []  # Lista de hammers
         self.recentImage = 0
         # Bandera de comprobacion para el lanzamiento de martillo
         self.flag = False
-        # Lista de posiciones del martillo
-        self.drawHammer = []
+        #cntador para cada tiempo que tiene que saltar el hammer
+        self.counter=0
+        self.down = True
+        self.timer = 0 #Tiempo para tirar el martillo
+        self.throw = False
+        self.lvl = 1
+
 
     # Esta funcion fue creada para guardar todos los martillos
     # Recibiendo paramatros de la posicion actual del martillo
-    def drawSmallHammer(self, posXH, posYH):
+    def saveSmallHammer(self, posXH, posYH):
         # Cracion de objeto killerHammer temporal
-        khammer = smallHammer(self.imageHammer, posXH, posYH)
+        khammer = smallHammer(self.imageSH, posXH, posYH)
         # Se agrega a la lista el objeto anterior para guardar el martillo
         self.listSmallHammers.append(khammer)
 
+    #Esta funcion es para el salto del Hammer segun el nivel
+    def jump(self, lvl):
+        if lvl > 1:
+            if self.down:
+                if self.posY > self.tamObject/ 3 :
+                    self.posY -= 30
+                else: self.down = False
+            else:
+                if self.rect.collidelist(self.listB) > -1:
+                    self.counter = 0
+                    self.down = True
+                else: self.posY += 30
+
+    def rectBricks(self, listBricks):
+        listRect = []
+        for brick in listBricks:
+            self.rectBrick = pygame.Rect((brick.rect.left, brick.rect.top),(brick.resolution))
+            # La superficie de los bricks se agregan a una lista
+            listRect.append(self.rectBrick)
+        return listRect
+
     def update(self):
         self.flag = False
+        self.throw = False
         # Tiempo que dura en permanecer una imagen
         if self.tamObject == 0:
             # Cambio de signo a la variable
             self.n = -(self.n)
             # Asignacion de su valor original
             self.tamObject = self.tamObjectCopy
-        # Nueva posicion en x del sprite
+        # Nueva posicion en x del Sprite
         self.posX -= self.n
         self.tamObject -= 1
+        if self.counter > 20:
+            self.jump(self.lvl)
+        if self.timer == 10: #comprueba el contador
+            self.throw = True
+            self.timer = 0 #Asigna de nuevo cero al timer
+        else:
+            self.timer += 1 # se incrementa si es falso
         # Para controlar el rango de la lista de imagenes
         if self.firstImage > self.lastImage:
-            # Se asigna cero para que recorra de nuevo la lista
-            self.firstImage = 0
+            self.firstImage = 0 # Se asigna cero para que recorra de nuevo la lista
             self.flag = True
         # Carga la siguiente imagen y se le asigana la posicion
         self.image = pygame.transform.scale(self.listImages[self.firstImage], self.size)
         self.image = pygame.transform.flip(self.image, True, False)
+        self.rect =self.image.get_rect()
+        self.rect.left = self.posX
+        self.rect.top = self.posY
         self.position = [self.posX, self.posY]
-        # Dibuja la imagen en la pantalla
-        self.firstImage += 1
+        self.firstImage += 1 #Incremento para la siguiente imagen
+        self.counter += 1 # Incremento en el contador para el salto
+
